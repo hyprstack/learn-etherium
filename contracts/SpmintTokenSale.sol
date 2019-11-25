e3cd8e3727af93b503e1e8d631819dbd78a5a13d
Mario <mario.c.mendes1985@gmail.com>
2019-11-25 15:43:20 +0000

    adds buyTokens functionality for token sale contract

--- contracts/SpmintTokenSale.sol
+++ contracts/SpmintTokenSale.sol
@@ -6,6 +6,9 @@ contract SpmintTokenSale {
     address admin;
     SpmintToken public tokenContract; // contract data type
     uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event Sell(address _buyer, uint256 _amount);
 
     constructor(SpmintToken _tokenContract, uint256 _tokenPrice) public {
         // Assign an admin
@@ -15,4 +18,25 @@ contract SpmintTokenSale {
         // Set Token Price
         tokenPrice = _tokenPrice;
     }
+
+    // multiply
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    // Buy Tokens
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        // require that value is equal to tokens
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        // require that the contract has enough tokens
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+        // require that a transfer is successful
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+        // keep track of tokens sold
+        tokensSold += _numberOfTokens;
+        // emit sell event
+        emit Sell(msg.sender, _numberOfTokens);
+    }
+
+    //
 }
--- test/SpmintTokenSale.js
+++ test/SpmintTokenSale.js
@@ -1,20 +1,65 @@
 const SpmintTokenSale = artifacts.require('./SpmintTokenSale.sol');
+const SpmintToken = artifacts.require('./SpmintToken.sol');
 
 contract('SpmintTokenSale', (accounts) => {
+  let tokenSaleInstance;
   let tokenInstance;
-  let tokenPrice = 1000000000000000; // in wei - smallest subdivion of ether to avoid using floats
+
+  const tokenPrice = 1000000000000000; // in wei - smallest subdivion of ether to avoid using floats
+  const admin = accounts[0];
+  const buyer = accounts[1];
+  const tokensAvailable = 750000;
+
   before(async () => {
-    tokenInstance = await SpmintTokenSale.deployed();
+    tokenInstance = await SpmintToken.deployed();
+    tokenSaleInstance = await SpmintTokenSale.deployed();
   });
 
   it('initialises the contract with the correct values', async () => {
-    const address = tokenInstance.address;
+    const address = tokenSaleInstance.address;
     assert.notEqual(address, 0x0, 'has contract address');
 
-    const tokenContractAddress = await tokenInstance.tokenContract();
+    const tokenContractAddress = await tokenSaleInstance.tokenContract();
     assert.notEqual(tokenContractAddress, 0x0, 'has token contract address');
 
-    const price = await tokenInstance.tokenPrice();
+    const price = await tokenSaleInstance.tokenPrice();
     assert.equal(price, tokenPrice, 'token price is correct');
+  });
+
+  it('facilitates token buying', async () => {
+    const numberOfTokens = 10;
+    const value = numberOfTokens * tokenPrice;
+
+    // provision 75% of all tokens to token sale
+    await tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, {from: admin});
+
+    const receipt = await tokenSaleInstance.buyTokens(numberOfTokens, {from: buyer, value});
+    assert.equal(receipt.logs.length, 1, 'triggers one event');
+    assert.equal(receipt.logs[0].event, 'Sell', 'should be the "Sell" event');
+    assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchased the tokens');
+    assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of tokens purchased');
+
+
+    const amount = await tokenSaleInstance.tokensSold();
+    assert.equal(amount.toNumber(), numberOfTokens, 'check the number of tokens sold is the same amount bought');
+
+    const buyersBalance = await tokenInstance.balanceOf(buyer);
+    assert.equal(buyersBalance.toNumber(), numberOfTokens, 'buyers tokens increases');
+
+    const balance = await tokenInstance.balanceOf(tokenSaleInstance.address);
+    assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens, 'tokenInstance token decreases');
+    // try to buy token different from the ether value
+    try {
+      assert.fail(await tokenSaleInstance.buyTokens(numberOfTokens, {from: buyer, value: 1}));
+    } catch (err) {
+      assert(err.message.includes('revert'), 'msg.value must equal number of tokens in wei');
+    }
+
+    // try to purchase more tokens than there are available in the contract
+    try {
+      assert.fail(await tokenSaleInstance.buyTokens(800000, {from: buyer, value: numberOfTokens * tokenPrice}));
+    } catch (err) {
+      assert(err.message.includes('revert'), 'cannot purchase more tokens than available');
+    }
   });
 });
